f956c1815287a554991cad359c820816fb994038
Lisa Hufnagel <lmhufnagel@gmail.com>
2017-07-31 03:18:26 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array
+  my_new_array = []
+end
+
+def array_with_two_elements
+  my_two_array = [1, 2]
+end
+
+def first_element (taylor_swift)
+  taylor_swift[0]
+end
+
+def third_element (taylor_swift)
+  taylor_swift[2]
+end
+
+def last_element (taylor_swift)
+  taylor_swift[-1]
+end
+
+def first_element_with_array_methods (array)
+  array.first
+end
+
+def last_element_with_array_methods (array)
+  array.last
+end
+
+def length_of_array (array)
+  array.count
+end
